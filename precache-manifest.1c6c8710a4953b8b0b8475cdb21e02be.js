d7d06ad25a2acaa871267fd0812d4824162c2221
Anujaditya02 <anujaditya02@gmail.com>
2020-05-12 18:52:31 +0530

    Updates

---
+++ precache-manifest.1c6c8710a4953b8b0b8475cdb21e02be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e53c0296170047a882d7745c5c4b50",
+    "url": "/Weather-App-React/index.html"
+  },
+  {
+    "revision": "0cbb26a1c6af303953f8",
+    "url": "/Weather-App-React/static/css/main.85304c9d.chunk.css"
+  },
+  {
+    "revision": "119f8b326d08107942e3",
+    "url": "/Weather-App-React/static/js/2.c831ed82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App-React/static/js/2.c831ed82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cbb26a1c6af303953f8",
+    "url": "/Weather-App-React/static/js/main.dd81e100.chunk.js"
+  },
+  {
+    "revision": "fc2750e7d73f8cd78e50",
+    "url": "/Weather-App-React/static/js/runtime-main.74be10e1.js"
+  },
+  {
+    "revision": "777b751fd661ea6c1a80651d2e10717c",
+    "url": "/Weather-App-React/static/media/densy.777b751f.jpg"
+  },
+  {
+    "revision": "160e400669f3ef3e58b62d3d952fa6e2",
+    "url": "/Weather-App-React/static/media/fog.160e4006.jpg"
+  }
+]);
